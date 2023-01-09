8751ab8ffa52deb1443a5fee0d00a18cd50e640a
Jacob Kozubek <kozubekjacob@gmail.com>
2023-01-09 16:34:04 +0100

    09.01.2022 add cors

---
+++ src/main/java/pl/kozubek/measuringstation/config/ApiConfiguration.java
@@ -0,0 +1,14 @@
+package pl.kozubek.measuringstation.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ApiConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*");
+    }
+}
